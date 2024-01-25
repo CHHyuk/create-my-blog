c6731fc6f401452b45619965c4689df1961b5569
CHHyuk <gksgur1038@naver.com>
2024-01-25 22:52:32 +0900

    feat: 레이아웃 타이틀 디자인 및 네비게이트 기능

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import GoogleButton from "./google-btn";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.section`
   display: flex;
@@ -11,18 +12,31 @@ const LayoutWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 15px 15px;
+  margin: 0 auto;
+  padding: 15px 15px;
+  max-width: 52.25rem;
+  width: 100%;
 `
 
-const SideBar = styled.nav`
-  
+const Title = styled.button`
+  font-size: 1.5rem;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  border: none; 
+  outline: none;
 `
 
 export default function Layout() {
+  const navigate = useNavigate();
+  const handleTitle = () => {
+    navigate("/");
+  }
+
   return (
     <Wrapper>
       <LayoutWrapper>
-        <SideBar>사이드바</SideBar>
+        <Title onClick={handleTitle}>웹개발 수난시대</Title>
         <GoogleButton />
       </LayoutWrapper>
       <Outlet />
