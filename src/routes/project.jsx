bbeade009e743df35889c03d8bf7ffef2262222c
CHHyuk <gksgur1038@naver.com>
2024-01-27 23:12:27 +0900

    feat: DB에서 tag, title 정보 가져오기

--- src/routes/project.jsx
+++ src/routes/project.jsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
-const projects = [
-  { id: `1`, title: 'project 1' },
-  { id: `2`, title: 'project 2' },
-  { id: `3`, title: 'project 3' },
-  { id: `4`, title: 'project 4' },
-]
-
+import { collection, query, orderBy, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,11 +13,23 @@ const Wrapper = styled.div`
 const TagList = styled.div`
   display: flex;
   justify-content: center;
-  gap: 0.3rem;
+  align-items: center;
+  gap: 0.5rem;
   font-size: 1.3rem;
   padding: 0 0 3rem 0;
-`
 
+`
+const Tag = styled.div`
+  border: 1px solid white;  
+  border-radius: 1rem;
+  padding: 0.5rem;
+  cursor: pointer;
+  &:hover {
+    background-color: white;
+    color: black;
+    transition: all 0.3s ease-in-out; 
+  }
+`
 const Container = styled.div`
   display: flex;
   border: 1px solid white;
@@ -40,12 +46,36 @@ const Items = styled.div`
 `
 
 export default function Project() {
+  const [projects, setProjects] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const querySnapshot = await getDocs(query(collection(db, 'project'), orderBy('createdAt', 'desc')));
+      const projectsArray = [];
+      const tagsSet = new Set();
+  
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        projectsArray.push({ id: doc.id, ...data });
+        if (data.tags) {
+          tagsSet.add(data.tags);
+        }
+      });
+  
+      setProjects(projectsArray);
+      setTags([...tagsSet]);
+    };
+  
+    fetchProjects();
+  }, []);
+
   return (
     <Wrapper>
       <TagList>
-        <tag>태그1</tag>
-        <tag>태그2</tag>
-        <tag>태그3</tag>
+        {tags.map((tag, index) => (
+          <Tag key={index}>{tag}</Tag>
+        ))}
       </TagList>
       <Container>
         {projects.map((project) => (
@@ -54,7 +84,6 @@ export default function Project() {
           </Items>
         ))}
       </Container>
-      <Outlet />
     </Wrapper>
   )
 }
