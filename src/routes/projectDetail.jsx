2648c7c4169c938d41e43bdd69894c4e476cc8ad
CHHyuk <gksgur1038@naver.com>
2024-01-28 16:31:12 +0900

    feat: 해당 id의 게시글 fetch로 가져와서 표현

--- src/routes/projectDetail.jsx
+++ src/routes/projectDetail.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 import styled from 'styled-components';
+import { Viewer } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 
 const Wrapper = styled.section`
   display: flex;
@@ -8,30 +12,55 @@ const Wrapper = styled.section`
   justify-content: center;
   align-items: center;
 `
-const Title = styled.title`
-  
+const Title = styled.div`
+  font-size: 2.5rem;
+  padding: 1rem;
 `
-const Contents = styled.div`
+const Tag = styled.div`
+  display: flex;
+  max-width: 52.25rem;
+  width: 80%;
+  justify-content: right;
+  align-items: center;
+  padding: 1rem;
+  font-size: 1.5rem;
+`
 
-`
-const Comments = styled.div`
-  
+const Contents = styled.div`
+  max-width: 52.25rem;
+  width: 100%;
 `
 
 export default function ProjectDetail() {
   const { id } = useParams();
+  const [project, setProject] = useState(null);
+
+  useEffect(() => {
+    const fetchProject = async () => {
+      const docRef = doc(db, 'project', id);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setProject({ id: docSnap.id, ...docSnap.data() });
+      } else {
+        console.log('No such document!');
+      }
+    };
+
+    fetchProject();
+  }, [id]);
+
+  if (!project) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Wrapper>
-      <Title>
-        제목 
-      </Title>
+      <Title>{project.title}</Title>
+      <Tag>{project.tag}</Tag>
       <Contents>
-        내용
+        <Viewer initialValue={project.content} />
       </Contents>
-      <Comments>
-        댓글
-      </Comments>
     </Wrapper>
   );
 }
